55ba653337f80cc7058a8ecd4e9764089ba75447
akimov.infotech <akimov.infotech@gmail.com>
2019-03-18 22:46:17 +0700

    add functionality to todo component

--- projects/todo/src/app/app.module.ts
+++ projects/todo/src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent], // why this needed
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
